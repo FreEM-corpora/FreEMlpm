1e88ee0462a7ea78b37acb4c7dc1413807c625fb
Simon Gabay <gabay.simon@gmail.com>
2026-09-03 21:00:44 +0200

    rename authority files

--- Authority_list/_clean_foreign.py
+++
@@ -1,5 +0,0 @@
-with open("foreign.tsv") as f:
-    data = sorted(list(set(f.read().split())))
-
-with open("foreign_processed.tsv", "w") as f:
-    f.write("\n".join(data))
--- Authority_list/_clean_propres.py
+++
@@ -1,5 +0,0 @@
-with open("propres.tsv") as f:
-    data = sorted(list(set(f.read().split())))
-
-with open("propres_processed.tsv", "w") as f:
-    f.write("\n".join(data))
